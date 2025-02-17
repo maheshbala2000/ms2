9f9cc8f243a9567cf7d30e7d981900b38b584045
Mahesh Balasubramanian <mahesh_bala2000@yahoo.com>
2025-02-17 12:52:17 +0530

    Updated logging code/config.

--- src/main/java/com/obviousai/Config.java
+++ src/main/java/com/obviousai/Config.java
@@ -1,26 +1,18 @@
 package com.obviousai;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableFeignClients
 public class Config {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-            .authorizeHttpRequests(auth -> auth
-//                .antMatchers("/payment/**").authenticated() // Require authentication for payment endpoints
-//                .antMatchers("/inventory/**")//.hasRole("ADMIN") // Require ADMIN role for inventory endpoints
-                .anyRequest().permitAll() // Allow all other requests
-            );
-
-        return http.build();
-    }
 
     @Bean
     public RestTemplate restTemplate() {
@@ -30,5 +22,16 @@ public class Config {
     @Bean
     public ModelMapper modelMapper() {
     	return new ModelMapper();
+    }
+    
+    @Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+    
+    @FeignClient(name="inventory-service", url="http://localhost:8082/")
+    public interface ValidateProduct {
+        @PostMapping("/validate/{productId}")
+        String validateAndLockStock();
     }
 }
--- src/main/java/com/obviousai/service/PaymentService.java
+++ src/main/java/com/obviousai/service/PaymentService.java
@@ -3,15 +3,21 @@ package com.obviousai.service;
 import java.util.Date;
 import java.util.UUID;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.obviousai.Config.ValidateProduct;
 import com.obviousai.domain.Payment;
 import com.obviousai.domain.PaymentDTO;
 import com.obviousai.dtos.InventoryDTO;
@@ -21,8 +27,16 @@ import com.obviousai.repository.PaymentRepository;
 @Service
 public class PaymentService {
 
+   Logger logger = LogManager.getLogger(getClass());
+	
    @Autowired
    private RestTemplate restTemplate;
+   @Autowired
+   @Lazy
+   private EurekaClient eurekaClient;
+   
+   @Autowired
+   ValidateProduct validateProduct;
 
    @Autowired
    private PaymentRepository paymentRepository;
@@ -43,12 +57,20 @@ public class PaymentService {
 	   
 	   ResponseEntity<InventoryDTO> ent=null;
 		try {
+			InstanceInfo info = eurekaClient.getApplication("inventory-service").
+					getInstances().get(0);
+			logger.info("Host/appname INFO::: "+info.getHostName()+":"+info.getPort()+"/"+info.getAppName());
+			// TODO: Doesn't work (404 error)
+			//			String res = validateProduct.validateAndLockStock();
+			logger.info("Now calling inventory service to validate & lock the stock.");
 			ent = restTemplate.postForEntity(inventoryServiceUrl + "/validate/" + paymentDTO.getOrderId(), 
-							   null, InventoryDTO.class);
+					null, InventoryDTO.class);
+
 			if(ent.getStatusCode().equals(HttpStatus.OK)) {
 				payment.setPaymentStatus(Payment.PaymentStatus.SUCCESS);
 			}
 		} catch (RestClientException e) {
+			logger.error("Exception occured while processing payment: " + e.getMessage());
 			payment.setPaymentStatus(Payment.PaymentStatus.FAILED);
 		}
        // Publish payment success event to MQ
